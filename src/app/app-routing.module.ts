af5c6d4b0abe721c025d03b79401aaa5c93499b5
Augusto Cesar <acsilvaluis@outlook.com>
2022-07-04 13:21:04 -0300

    Prototipo feito, paginas ligadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutUs/aboutus.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
@@ -10,6 +13,9 @@ const routes: Routes = [
 
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'aboutUs', component: AboutusComponent },
+  { path: 'contact', component: ContactComponent },
 
 ];
 

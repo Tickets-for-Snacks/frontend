1c2334a1a2dce8590db5e376819bff2beaf8da39
elenilson <sousalele99@icloud.com>
2022-07-07 10:33:28 -0300

    Login Finalizado

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UsuarioLogin } from '../model/UsuarioLogin';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private router :Router
+
+  ) { }
+
+  login(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin>{
+    return this.http.post<UsuarioLogin>('https://ticketsforsnacks.herokuapp.com/usuarios/logar', usuarioLogin)
+  }
+
 }

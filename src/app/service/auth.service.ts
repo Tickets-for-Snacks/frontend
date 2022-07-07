c38788731eee56133e8acbab57f8019d3c263e3b
Vitor <vitorhaguilar@gmail.com>
2022-07-07 12:10:48 -0300

    cadastro

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from '../model/Usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  register(user: Usuario): Observable<Usuario> {
+    return this.http.post<Usuario>('https://ticketsforsnacks.herokuapp.com/usuarios/cadastrar', user)
+  }
 }

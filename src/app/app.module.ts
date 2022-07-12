60809d996614b54fd2e7c0ae52e9261e7fc57917
Augusto Cesar <acsilvaluis@outlook.com>
2022-07-12 13:45:07 -0300

    Organização de pastas e criação de modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
+import { MenuComponent } from './allMenu/menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { AboutusComponent } from './aboutUs/aboutus.component';
-import { ContactComponent } from './contact/contact.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './mainPages/home/home.component';
+import { AboutusComponent } from './mainPages/aboutUs/aboutus.component';
+import { ContactComponent } from './mainPages/contact/contact.component';
+import { RegisterComponent } from './mainPages/register/register.component';
 import { ProductComponent } from './product/product.component';
-import { ProdutoEditComponent } from './edit/produto-edit/produto-edit.component';
-import { CategoriaEditComponent } from './edit/categoria-edit/categoria-edit.component';
-import { UsuarioEditComponent } from './edit/usuario-edit/usuario-edit.component';
-import { CategoriaDeleteComponent } from './delete/categoria-delete/categoria-delete.component';
-import { ProdutoDeleteComponent } from './delete/produto-delete/produto-delete.component';
-import { MenuAfterLoginComponent } from './menu-after-login/menu-after-login.component';
-import { ProfileComponent } from './profile/profile.component';
-import { UserProductComponent } from './user-product/user-product.component';
+import { CategoriaEditComponent } from './profile/user-category/categoria-edit.component';
+import { MenuAfterLoginComponent } from './allMenu/menu-after-login/menu-after-login.component';
+import { UserProductComponent } from './profile/user-product/user-product.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ProfileComponent } from './profile/user-profile/profile.component';
+import { LoginComponent } from './mainPages/login/login.component';
+
+
+
 
 @NgModule({
   declarations: [
@@ -31,14 +29,10 @@ import { UserProductComponent } from './user-product/user-product.component';
     HomeComponent,
     AboutusComponent,
     ContactComponent,
-    RegisterComponent,
     LoginComponent,
+    RegisterComponent,
     ProductComponent,
-    ProdutoEditComponent,
     CategoriaEditComponent,
-    UsuarioEditComponent,
-    CategoriaDeleteComponent,
-    ProdutoDeleteComponent,
     MenuAfterLoginComponent,
     ProfileComponent,
     UserProductComponent

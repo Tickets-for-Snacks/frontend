b28395733cfdfeb72a5e30c2a9b99e7760201a83
Augusto Cesar <acsilvaluis@outlook.com>
2022-07-12 09:43:38 -0300

    Pag Produtos, Começo do CRUD

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutusComponent } from './aboutUs/aboutus.component';
 import { ContactComponent } from './contact/contact.component';
+import { CategoriaDeleteComponent } from './delete/categoria-delete/categoria-delete.component';
+import { ProdutoDeleteComponent } from './delete/produto-delete/produto-delete.component';
+import { CategoriaEditComponent } from './edit/categoria-edit/categoria-edit.component';
+import { ProdutoEditComponent } from './edit/produto-edit/produto-edit.component';
+import { UsuarioEditComponent } from './edit/usuario-edit/usuario-edit.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { ProductComponent } from './product/product.component';
@@ -18,6 +23,12 @@ const routes: Routes = [
   { path: 'aboutUs', component: AboutusComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'product', component: ProductComponent },
+
+  { path: 'categoria-edit', component: CategoriaEditComponent },
+  { path: 'produto-edit', component: ProdutoEditComponent },
+  { path: 'usuario-edit', component: UsuarioEditComponent },
+  { path: 'categoria-delete', component: CategoriaDeleteComponent },
+  { path: 'produto-delete', component: ProdutoDeleteComponent },
 
 ];
 

4dbbaeb8b490c50cec348e4bc33a3d3a40686416
Augusto Cesar <acsilvaluis@outlook.com>
2022-07-18 01:09:37 -0300

    Estilização da pagina de Produto ID

--- src/app/product/pag-ticket/pag-ticket.component.ts
+++ src/app/product/pag-ticket/pag-ticket.component.ts
@@ -1,47 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import arrayShuffle from 'array-shuffle';
 import { Produto } from 'src/app/model/Produto';
 import { ProdutoService } from 'src/app/service/produto.service';
 
 @Component({
   selector: 'app-pag-ticket',
   templateUrl: './pag-ticket.component.html',
-  styleUrls: ['./pag-ticket.component.css']
+  styleUrls: ['./pag-ticket.component.css'],
 })
 export class PagTicketComponent implements OnInit {
+  produto: Produto = new Produto();
 
-  produto: Produto = new Produto()
-  idProduto: number
+  listaProdutos: Produto[];
+
+  idProduto: number;
 
   constructor(
     private produtoService: ProdutoService,
     private router: Router,
     private route: ActivatedRoute
-  ) { }
+  ) {}
 
   ngOnInit() {
+    this.idProduto = this.route.snapshot.params['id'];
+    this.findByIdProduto(this.idProduto);
 
-    this.idProduto = this.route.snapshot.params['id']
-    this.findByIdProduto(this.idProduto)
-
+    this.getAllProdutos();
   }
 
-  findByIdProduto(id: number){
+  findByIdProduto(id: number) {
     this.produtoService.getByIdProduto(id).subscribe((resp: Produto) => {
-      this.produto = resp
-    })
+      this.produto = resp;
+    });
+    this.getAllProdutos()
+  }
+
+  getAllProdutos() {
+    this.produtoService.getAllProduto().subscribe((resp: Produto[]) => {
+      this.listaProdutos = resp;
+      let shuffle = arrayShuffle(this.listaProdutos);
+      this.listaProdutos = shuffle;
+    });
+  }
+
+  getProdutoByCategoria(itemProdutos: Produto[]) {
+    this.listaProdutos = itemProdutos;
   }
 
   verificaImagem(event: Event) {
     const htmlImagem = event.target as HTMLImageElement;
     htmlImagem.src =
-      'https://cdn5.vectorstock.com/i/1000x1000/73/49/404-error-page-not-found-miss-paper-with-white-vector-20577349.jpg';
+      'https://i.imgur.com/UB7iHgs.png';
   }
 
-  comprar(){
-    alert('Ticket adicionado no carrinho!')
+  comprar() {
+    alert('Ticket adicionado no carrinho!');
   }
-
 }
-
-
